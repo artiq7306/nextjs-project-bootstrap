fbfef0471e7d16c3feee392613f53990a98f4191
artiq7306 <artiqsaparniyazov0@gmail.com>
2025-07-19 01:37:15 +0300

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1752885423288

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_next_dist_c3a575e9._.js",
+  "static/chunks/src_components_layout_Navbar_tsx_d21b0d54._.js"
+],
+    source: "dynamic"
+});

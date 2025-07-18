1edfa895bc668d4f61fa3a62ef937b5a84684c54
artiq7306 <artiqsaparniyazov0@gmail.com>
2025-07-19 01:25:26 +0300

    Add .next/static/chunks/src_app_page_tsx_7e55577e._.js 1752884713878

---
+++ .next/static/chunks/src_app_page_tsx_7e55577e._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d66391fb._.js",
+  "static/chunks/src_634c12aa._.js"
+],
+    source: "dynamic"
+});
